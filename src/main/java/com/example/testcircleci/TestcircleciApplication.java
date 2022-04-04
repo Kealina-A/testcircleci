bb5aaf283d34582fdd8deef28dd7d35e97492c45
Kealina <kealina@qq.com>
2022-04-04 14:00:35 +0800

    first commit

---
+++ src/main/java/com/example/testcircleci/TestcircleciApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcircleciApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(TestcircleciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testcircleci/TestcircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcircleciApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
